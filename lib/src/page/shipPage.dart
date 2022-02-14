12b10f7026a3cf42cac44f3e1d283d70be2bfe75
ukkpaapay <ukristkrit@hotmail.com>
2022-02-14 11:31:35 +0700

    change card ship page

--- lib/src/page/shipPage.dart
+++ lib/src/page/shipPage.dart
@@ -16,34 +16,60 @@ _Card(color) {
     },
     child: Container(
       margin: EdgeInsets.only(top: 20),
-      height: 75,
+      height: 80,
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.all(Radius.circular(10)),
         boxShadow: [
           BoxShadow(
-            color: color,
-            spreadRadius: 2,
+            color: AppTheme.light.shadowColor,
+            spreadRadius: 0,
             blurRadius: 6,
             offset: Offset(0, 1), // changes position of shadow
           ),
         ],
       ),
       child: Container(
-        margin: EdgeInsets.only(left: 75),
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.only(left: 50),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(100),
+              // bottomLeft: Radius.circular(100),
               bottomRight: Radius.circular(10),
               topRight: Radius.circular(10)),
         ),
-        child: Center(
-          child: Text(
-            "Helix Test",
-            style: TextStyle(
-                color: Colors.black, fontSize: 24, fontWeight: FontWeight.w700),
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "เรือประมงพาณิชย์",
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "Gen 1",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400),
+                ),
+                Text(
+                  "Helix Test",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     ),
@@ -54,31 +80,36 @@ class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        // backgroundColor: Colors.transparent,
+        // elevation: 0,
+        title: Center(
+            child: Text(
+          "Ship",
+          style: TextStyle(color: Colors.black),
+        )),
+      ),
       body: Container(
         padding: EdgeInsets.all(18),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(23)),
-                  color: AppTheme.light.primaryColor,
-                ),
-                width: double.infinity,
-                height: 200,
-                child: Center(
-                  child: Text(
-                    "จำนวนเรือทั้งหมด 20 ลำ",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700),
-                  ),
-                )),
+              margin: EdgeInsets.only(right: 8),
+              child: Text(
+                "จำนวนเรือ 2 ลำ",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
             Flexible(
               child: ListView(
                 children: <Widget>[_Card(Colors.blue), _Card(Colors.red)],
               ),
-            )
+            ),
           ],
         ),
       ),
