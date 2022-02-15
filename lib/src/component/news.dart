72c88d3f09fc471d830d1a88cc225af71ff7800d
etnecadev <etneca.dev@gmail.com>
2022-02-15 15:50:30 +0700

    slove yellow underline text

--- lib/src/component/news.dart
+++ lib/src/component/news.dart
@@ -6,57 +6,60 @@ class NewsComponent extends StatelessWidget {
     static const routeName = '/news';
   @override
   Widget build(BuildContext context) {
-    return AnimationLimiter(
-      child: GridView.custom(
-          gridDelegate: SliverWovenGridDelegate.count(
-            crossAxisCount: 2,
-            // mainAxisSpacing: 5,
-            crossAxisSpacing: 7,
+    return Material(
+      
+          child: AnimationLimiter(
+        child: GridView.custom(
+            gridDelegate: SliverWovenGridDelegate.count(
+              crossAxisCount: 2,
+              // mainAxisSpacing: 5,
+              crossAxisSpacing: 7,
 
-            pattern: [
-              WovenGridTile(0.9),
-              WovenGridTile(
-                5 / 7,
-                crossAxisRatio: 1,
-                alignment: AlignmentDirectional.centerEnd,
-              ),
-            ],
-          ),
-          childrenDelegate: SliverChildBuilderDelegate(
-              (context, index) => AnimationConfiguration.staggeredGrid(
-                    position: index,
-                    duration: const Duration(milliseconds: 400),
-                    columnCount: 2,
-                    child: ScaleAnimation(
-                      child: FadeInAnimation(
-                        child: Container(
-                          child: Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Text(
-                                
-                                  "'กรมเจ้าท่า'ไฟเขียว!รับอุทธรณ์ประมงพื้นบ้านกว่า 9 พันลำ ให้ตีทะเบียนใหม่ ขีดเส้น 15 วัน",
-                                  softWrap: true,
-                                  overflow: TextOverflow.fade,
-                                  textAlign: TextAlign.justify,
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.w800)),
+              pattern: [
+                WovenGridTile(0.9),
+                WovenGridTile(
+                  5 / 7,
+                  crossAxisRatio: 1,
+                  alignment: AlignmentDirectional.centerEnd,
+                ),
+              ],
+            ),
+            childrenDelegate: SliverChildBuilderDelegate(
+                (context, index) => AnimationConfiguration.staggeredGrid(
+                      position: index,
+                      duration: const Duration(milliseconds: 400),
+                      columnCount: 2,
+                      child: ScaleAnimation(
+                        child: FadeInAnimation(
+                          child: Container(
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  
+                                    "'กรมเจ้าท่า'ไฟเขียว!รับอุทธรณ์ประมงพื้นบ้านกว่า 9 พันลำ ให้ตีทะเบียนใหม่ ขีดเส้น 15 วัน",
+                                    softWrap: true,
+                                    overflow: TextOverflow.fade,
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.w800)),
+                              ),
                             ),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                    image: AssetImage(
+                                        'assets/images/news${index + 1}.png'),
+                                    fit: BoxFit.fill)),
                           ),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              image: DecorationImage(
-                                  image: AssetImage(
-                                      'assets/images/news${index + 1}.png'),
-                                  fit: BoxFit.fill)),
                         ),
                       ),
                     ),
-                  ),
-              childCount: 5)),
+                childCount: 5)),
+      ),
     );
   }
 }
